d6b0925b99b76544b6fe5a367fde069a53e5c544
usmani391 <manidevolper96@gmail.com>
2023-12-18 22:16:11 +0500

    finallt tmain page and Resources are completed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['upload.wikimedia.org','wallpapercave.com','kotlinlang.org','about.codecov.io'],
+    domains: ['upload.wikimedia.org','wallpapercave.com','kotlinlang.org','about.codecov.io','www.newma.co.uk'],
   },
 }
 
