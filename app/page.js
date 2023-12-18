320ba786ec600209fa91b4f1284207e2072c7fd8
usmani391 <manidevolper96@gmail.com>
2023-12-18 14:44:34 +0500

    icon libirary

--- app/page.js
+++ app/page.js
@@ -2,16 +2,16 @@ import Banner from "@/components/Banner";
 import Top10 from "@/components/Top10";
 import Section2 from "@/components/Section2";
 import Navbar from "@/components/Nav";
-import Section3 from "@/components/Section3"
-const Home=() =>{
+import Section3 from "@/components/Section3";
+const Home = () => {
   return (
     <section>
-      <Navbar val='Home'/>
+      <Navbar val="Home" />
       <Banner />
       <Section2 />
-      <Section3/>
+      <Section3 />
       <Top10 />
     </section>
   );
-}
- export default Home ;
+};
+export default Home;
